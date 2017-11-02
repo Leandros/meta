74b5faf7468702a3f6faf53359ab5c65c3c8ecd3
Arvid Gerstmann <dev@arvid-g.de>
2017-11-02 18:35:53 +0100

    Extend platform.hxx

--- include/a/platform.hxx
+++ include/a/platform.hxx
@@ -22,57 +22,153 @@
 
 /* ========================================================================= */
 /* Utilities                                                                 */
-#define A_USING(x)      ((1 x 1) == 2)
-#define A_ON            +
-#define A_OFF           -
+#define USING(x)                ((1 x 1) == 2)
+#define ON                      +
+#define OFF                     -
 
 
 /* ========================================================================= */
 /* Platform Macros                                                           */
-#define A_WINDOWS       A_OFF
-#define A_MAC           A_OFF
-#define A_LINUX         A_OFF
-#define A_BSD           A_OFF
+/* ========================================================================= */
+#define OS_WINDOWS              OFF
+#define OS_MAC                  OFF
+#define OS_LINUX                OFF
+#define OS_BSD                  OFF
+#define OS_IPHONE               OFF
+#define OS_ANDROID              OFF
+#define OS_POSIX                OFF
+/* Bitness: */
+#define OS_32BIT                OFF
+#define OS_64BIT                OFF
 
 /* ------------------------------------------------------------------------- */
 /* Windows                                                                   */
-#if defined(_WIN64) \
-    || defined(_WIN32) \
-    || defined(__WIN32__) \
-    || defined(__WINDOWS__)
+#if    defined(_WIN32) || defined(__WIN32__)        \
+    || defined(_WINDOWS) || defined(__WINDOWS__)    \
+    || defined(__TOS_WIN__)
 
-#undef  A_WINDOWS
-#define A_WINDOWS       A_ON
+    #undef  OS_WINDOWS
+    #define OS_WINDOWS          ON
 
+    #if defined(_WIN64)
+        #undef  OS_64BIT
+        #define OS_64BIT        ON
+    #else
+        #undef  OS_32BIT
+        #define OS_32BIT        ON
+    #endif
 #endif
 
 /* ------------------------------------------------------------------------- */
 /* macOS                                                                     */
 #if defined(__APPLE__)
+    #include <TargetConditionals.h>
 
-#undef  A_MAC
-#define A_MAC           A_ON
+    #if TARGET_IPHONE_SIMULATOR || TARGET_OS_IPHONE
+        #undef  OS_IPHONE
+        #define OS_IPHONE       ON
+        #if defined(__LP64__) || defined(_LP64)
+            #undef  OS_64BIT
+            #define OS_64BIT    ON
+        #else
+            #undef  OS_32BIT
+            #define OS_32BIT    ON
+        #endif
 
+    #elif TARGET_OS_MAC
+        #undef  OS_MAC
+        #define OS_MAC          ON
+        #undef  OS_64BIT
+        #define OS_64BIT        ON
+    #endif
 #endif
 
 /* ------------------------------------------------------------------------- */
 /* Linux                                                                     */
-#if defined(__linux__)
+#if    defined(LINUX) || defined(linux) || defined(__linux__) || defined(__linux) \
+    || defined(__unix__) || defined(__unix)
 
-#undef  A_LINUX
-#define A_LINUX         A_ON
+    #if defined(__ANDROID__)
+        #undef  OS_ANDROID
+        #define OS_ANDROID      ON
+        /* TODO: Detect OS bitness! */
+    #else
+        #undef  OS_LINUX
+        #define OS_LINUX        ON
 
+        #if    defined(__amd64__) || defined(__amd64) \
+            || defined(__x86_64__) || defined(__x86_64)
+            #undef  OS_64BIT
+            #define OS_64BIT    ON
+        #else
+            #undef  OS_32BIT
+            #define OS_32BIT    ON
+        #endif
+    #endif
 #endif
 
 /* ------------------------------------------------------------------------- */
 /* BSD                                                                       */
-#if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__)
-
-#undef  A_BSD
-#define A_BSD           A_ON
-
+#if    defined(FreeBSD) || defined(__FreeBSD__)     \
+    || defined(NetBSD) || defined(__NetBSD__)       \
+    || defined(OpenBSD) || defined(__OpenBSD__)     \
+    || defined(__bsdi__) || defined(__DragonFly__)
+    #undef  OS_BSD
+    #define OS_BSD              ON
 #endif
 
+/* ------------------------------------------------------------------------- */
+/* POSIX                                                                     */
+#if USING(OS_MAC) || USING(OS_LINUX) || USING(OS_BSD)
+    #undef  OS_POSIX
+    #define OS_POSIX            ON
+#endif
+
+
+/* ========================================================================= */
+/* Compiler Macros                                                           */
+/* ========================================================================= */
+#define COMPILER_GCC            OFF
+#define COMPILER_ICC            OFF
+#define COMPILER_GHS            OFF
+#define COMPILER_CLANG          OFF
+#define COMPILER_MSVC           OFF
+
+
+/* ------------------------------------------------------------------------- */
+/* gcc                                                                       */
+#if defined(__GNUC__) && !defined(__clang__)
+    #undef  COMPILER_GCC
+    #define COMPILER_GCC        ON
+#endif
+
+/* ------------------------------------------------------------------------- */
+/* icc                                                                       */
+#if defined(__INTEL_COMPILER) || defined(__ICC) || defined(__ECC)
+    #undef  COMPILER_ICC
+    #define COMPILER_ICC        ON
+#endif
+
+/* ------------------------------------------------------------------------- */
+/* green hills                                                               */
+#if defined(__ghs__)
+    #undef  COMPILER_GHS
+    #define COMPILER_GHS        ON
+#endif
+
+/* ------------------------------------------------------------------------- */
+/* clang                                                                     */
+#if defined(__clang__)
+    #undef  COMPILER_CLANG
+    #define COMPILER_CLANG      ON
+#endif
+
+/* ------------------------------------------------------------------------- */
+/* MSVC                                                                      */
+#if defined(_MSC_VER)
+    #undef  COMPILER_MSVC
+    #define COMPILER_MSVC       ON
+#endif
 
 #endif /* A_PLATFORM_HXX */
 
