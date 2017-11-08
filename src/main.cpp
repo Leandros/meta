7dd6d6db67f2b5a592c60e99bcea78e729c71b8c
Arvid Gerstmann <dev@arvid-g.de>
2017-11-08 11:54:56 +0100

    Add option parsing
    Options passed to meta.exe will now be properly parsed, usage is yet
    to be added.
    New options can be added via the 'longopts' array of structures.

--- src/main.cpp
+++ src/main.cpp
@@ -22,9 +22,81 @@
 #include <meta/parser.hpp>
 #include <meta/internal/clang.hpp>
 
-int
-main(void)
+#define OPTPARSE_IMPLEMENTATION
+#define OPTPARSE_API static
+#include <a/optparse/optparse.h>
+
+
+/* ========================================================================= */
+/* Options                                                                   */
+/* ========================================================================= */
+#define ARR_SIZE(x) (sizeof(x) / sizeof(x[0]))
+static struct optparse_long longopts[] = {
+    /*
+     * 1st arg is long option name. can be null.
+     * 2nd arg is short option name. can be null.
+     * 3rd arg controls the argument of option. Must be one of:
+     *      OPTPARSE_NONE, OPTPARSE_REQUIRED, OPTPARSE_OPTIONAL
+     * 4th arg is a short description, printed in help. (EXTENSION!)
+     *
+     * Arguments may only be added to the end! They're accessed by index!
+     */
+    { "help", '?', OPTPARSE_NONE, "Print help and exit" },
+    { "version", 0, OPTPARSE_NONE, "Print version and exit" },
+    { 0, 'I', OPTPARSE_REQUIRED, "Add include directory" },
+    { "llvm", 0, OPTPARSE_REQUIRED, "Path to LLVM installation, if non-default" },
+    { 0, 0, OPTPARSE_NONE, 0 }
+};
+
+static bool opt_help = false;
+static bool opt_version = false;
+static char const *opt_llvm = ".";
+
+static void print_help(char const *name, bool exit = true);
+static void print_version(char const *name);
+static void add_include(char const *path);
+
+static int
+parseOptions(int, char **argv)
 {
+    int option, index;
+    struct optparse options;
+
+    optparse_init(&options, argv);
+    while ((option = optparse_long(&options, longopts, &index)) != -1) {
+        switch (index) {
+        case 0: opt_help = true; break;
+        case 1: opt_version = true; break;
+        case 2: add_include(options.optarg); break;
+        case 3: opt_llvm = options.optarg; break;
+        default:
+            fprintf(stderr, "%s: %s\n", argv[0], options.errmsg);
+            print_help(argv[0], false);
+            return false;
+        }
+    }
+
+    if (opt_help)
+        print_help(argv[0], true);
+    if (opt_version)
+        print_version(argv[0]);
+
+    return options.optind;
+}
+
+
+/* ========================================================================= */
+/* ENTRY                                                                     */
+/* ========================================================================= */
+int
+main(int argc, char **argv)
+{
+    int optind;
+    if ((optind = parseOptions(argc, argv)) == -1)
+        return 1;
+    if (optind == argc)
+        return fprintf(stderr, "error: requires file argument\n"), 1;
+
 #if 0
     a::process proc;
 
@@ -41,11 +113,12 @@ main(void)
     }
 #endif
 
-    meta::parser p;
-    if (!p.init_from_file("test.h"))
-        return 1;
-
-    p.run();
+    for (int i = optind; i < argc; ++i) {
+        meta::parser p;
+        if (!p.init_from_file(argv[i]))
+            return 1;
+        p.run();
+    }
 
 
 #if 0
@@ -62,5 +135,40 @@ main(void)
 #endif
 
     return 0;
+}
+
+
+/* ========================================================================= */
+/* Help / Version                                                            */
+/* ========================================================================= */
+static void
+print_help(char const *name, bool shall_exit)
+{
+    printf("USAGE: %s [OPTION...] FILE...\n", name);
+    printf("\n");
+    printf("OPTIONS:\n");
+    printf("\n");
+
+    optparse_print_help(longopts);
+
+    if (shall_exit)
+        exit(EXIT_SUCCESS);
+}
+
+static void
+print_version(char const *name)
+{
+    printf("%s v%s (%s)\n", name, "1.0.0", "debug");
+    printf("Copyright (C) 2017 Arvid Gerstmann\n");
+    exit(EXIT_SUCCESS);
+}
+
+static void
+add_include(char const *path)
+{
+    if (path == nullptr)
+        return;
+
+    printf("add_include(%s)\n", path);
 }
 
